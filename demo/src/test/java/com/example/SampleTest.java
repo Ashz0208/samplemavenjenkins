e04246c6bec7152cdc3436c00489462ccfdccf82
neojarvis <techadmin@iamneo.ai>
2025-03-27 13:37:07 +0000

    my initial code

---
+++ demo/src/test/java/com/example/SampleTest.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+     @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        System.out.println("Testacse passed");
+    }
+    
+}
